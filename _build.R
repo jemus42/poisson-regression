8f6c96152a713460832d30591b9cba7de5191932
Lukas Burk <lukas@quantenbrot.de>
2019-08-14 15:36:03 +0200

    Change output name, add build script

--- _build.R
+++ _build.R
@@ -1,9 +1,20 @@
 #! /usr/bin/env Rscript
-# Build things
+# 1. Update source from git repository and clean up previous output
+# 2. Build the project (HTML + PDF output)
+# 3. Copy output to destination folder
+
+cat("1. Pulling from git repository\n")
+git2r::pull()
+
+cat("Removing previously build output\n")
+if (fs::dir_exists("poisson-regression"))  fs::dir_delete("poisson-regression")
+
 
 # Gitbook
-bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook")
+cat("2.1 Rendering gitbook (HTML site)\n")
+bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", envir = new.env())
 
 # PDF
-bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
+cat("2.2 Rendering PDF)\n")
+bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book", envir = new.env())
 
