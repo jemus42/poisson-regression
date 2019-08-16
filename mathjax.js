414558ae2b9a880e3595ee838dce90f1d8b7a62b
Lukas Burk <lukas@quantenbrot.de>
2019-08-16 09:51:30 +0200

    Tweak CSS, mathjax

---
+++ mathjax.js
@@ -0,0 +1,7 @@
+<script type="text/x-mathjax-config">
+  MathJax.Hub.Config({
+    "HTML-CSS": { fonts: ["Asana-Math"] },
+    MathMenu: { showFontMenu: true },
+    menuSettings: { zoom: "Click" }
+  });
+</script>
