5ae7b09d7bad39d02f882d59e36a66e264f81a81
Lukas Burk <lukas@quantenbrot.de>
2019-08-16 09:51:50 +0200

    Mild restructuring

--- _build.R
+++ _build.R
@@ -4,18 +4,25 @@
 # 2. Build the project (HTML + PDF output)
 # 3. Copy output to destination folder
 
-#cat("1.1 Pulling from git repository\n")
-# it2r::pull(credentials = git2r::cred_ssh_key())
+# cat("1.1 Pulling from git repository\n")
+# git2r::pull(credentials = git2r::cred_ssh_key())
 
+timestamp()
 
 cat("1.2 Removing previously build output\n")
 if (fs::dir_exists("poisson-regression"))  fs::dir_delete("poisson-regression")
 
 # Gitbook
 cat("2.1 Rendering gitbook (HTML site)\n")
-bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", envir = new.env())
+bookdown::render_book(
+  "index.Rmd", output_format = "bookdown::gitbook", envir = new.env(), quiet = TRUE
+)
 
 # PDF
 cat("2.2 Rendering PDF)\n")
-bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book", envir = new.env())
+bookdown::render_book(
+  "index.Rmd", output_format = "bookdown::pdf_book", envir = new.env(), quiet = TRUE
+)
 
+cat("Done rendering!\n")
+timestamp()
