6b13659f2537e17a27f9f09c8462bf025e7a40dd
Lukas Burk <lukas@quantenbrot.de>
2019-08-30 14:37:43 +0200

    Meh, good enough for now

--- _build.R
+++ _build.R
@@ -6,37 +6,38 @@
 
 # cat("1.1 Pulling from git repository\n")
 # git2r::pull(credentials = git2r::cred_ssh_key())
+library(cliapp)
 
 timestamp()
 
-cat("1.2 Removing previously built output\n")
+cli_h2("1.2 Removing previously built output")
 if (fs::dir_exists("poisson-regression"))  fs::dir_delete("poisson-regression")
 if (fs::file_exists("poisson-regression.Rmd")) fs::file_delete("poisson-regression.Rmd")
 if (fs::dir_exists("poisson-regression_files"))  fs::dir_delete("poisson-regression_files")
 
 # Gitbook
-cat("2.1 Rendering HTML site\n")
+cli_h2("2.1 Rendering HTML site")
 bookdown::render_book(
   "index.Rmd", output_format = "bookdown::gitbook", envir = new.env(), quiet = TRUE
 )
 
 # PDF
-cat("2.2 Rendering PDF\n")
+cli_h2("2.2 Rendering PDF")
 bookdown::render_book(
   "index.Rmd", output_format = "bookdown::pdf_book", envir = new.env(), quiet = TRUE
 )
 
-cat("Done rendering!\n")
+cli_alert_success("Done rendering!")
 
 if (all(Sys.info()[c("sysname", "login")] == c("Windows", "burk"))) {
-  cat("Copying to G:\\EVERYONE\\Burk...\n")
+  cli_h2("Copying to G:\\EVERYONE\\Burk...")
   fs::dir_copy("poisson-regression", "G:/EVERYONE/Burk/poisson-regression", overwrite = TRUE)
 }
 
 if (all(Sys.info()[c("sysname", "login")] == c("Darwin", "Lukas"))) {
-  cat("Copying to lukas.tadaa-data.de/poisson/\n")
+  cli_h2("Copying to lukas.tadaa-data.de/poisson/")
   fs::dir_copy("poisson-regression", "~/Sync/lukas.tadaa-data.de/poisson/", overwrite = TRUE)
 }
 
-cat("Done!\n")
+cli_alert_success("Done!")
 timestamp()
