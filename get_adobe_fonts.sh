f4224f1fb634563d8c53a990081bffe50b1f8719
Lukas Burk <lukas@quantenbrot.de>
2020-03-07 00:46:46 +0100

    Maybe Source Pro fonts?

---
+++ get_adobe_fonts.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# Do the font dance with Source Pro (all of them)
+# Adapted from: https://gist.github.com/enzinier/8d00d3f37d2e23985dcfa65662d163fa
+
+FONT_HOME="$HOME/.local/share/fonts"
+
+echo "installing fonts at $PWD to $FONT_HOME"
+
+VARIANTS=(serif sans source)
+
+for VARIANT in $VARIANTS; do
+
+  echo "Downloading Source >$VARIANT< Pro"
+  # Create directory to hold the fonts
+  mkdir -p "$FONT_HOME/adobe-fonts/source-$VARIANT-pro"
+
+  # Download the fonts by cloning the repos (they're not *that* big)
+  (git clone \
+   --branch release \
+   --depth 1 \
+   'https://github.com/adobe-fonts/source-$VARIANT-pro.git' \
+   "$FONT_HOME/adobe-fonts/source-$VARIANT-pro")
+
+   # Refresh font cache to make fonts available
+   fc-cache -f -v "$FONT_HOME/adobe-fonts/source-$VARIANT-pro"
+
+done
